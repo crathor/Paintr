f48c6a48ddb202ba837b5a7f61a68202f80a24a9
Cody Rathor <codyrathor@gmail.com>
2018-08-01 15:10:46 -0700

    removed unnecessary console.logs

--- modules/ui/containers/App/index.js
+++ modules/ui/containers/App/index.js
@@ -44,7 +44,6 @@ class App extends Component {
   }
   render() {
     const { players } = this.props
-    console.log(this.props.players)
     return (
       <Stage
         width={window.innerWidth}
