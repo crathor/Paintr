f67ba31ee43b78ac331c3af23039c2f159d5154d
johnstonstu <stu.w.johnston@gmail.com>
2018-08-08 16:18:31 -0700

    styled controller page name form

--- modules/ui/components/controller/index.js
+++ modules/ui/components/controller/index.js
@@ -1,56 +1,58 @@
-import React, { Component } from 'react'
-import Joystick from './Joystick'
-import { Meteor } from 'meteor/meteor'
+import React, { Component } from "react";
+import Joystick from "./Joystick";
+import { Meteor } from "meteor/meteor";
+import "./styles.css";
 
 class Controller extends Component {
   state = {
     playerCreated: false,
-    name: '',
+    name: "",
     player: {}
-  }
+  };
   handleChange = event => {
-    if (event.target.value.length > 8) return
-    this.setState({ name: event.target.value })
-  }
+    if (event.target.value.length > 8) return;
+    this.setState({ name: event.target.value });
+  };
 
   handleSubmit = e => {
-    e.preventDefault()
-    if (this.state.name === '') return
+    e.preventDefault();
+    if (this.state.name === "") return;
     this.setState(
       {
         playerCreated: true
       },
       async () => {
-        await Meteor.call('add.player', this.state.name)
-        Meteor.call('get.player', Meteor.userId(), (err, res) => {
-          this.setState({ player: res })
-        })
+        await Meteor.call("add.player", this.state.name);
+        Meteor.call("get.player", Meteor.userId(), (err, res) => {
+          this.setState({ player: res });
+        });
       }
-    )
-  }
+    );
+  };
   render() {
-    const { playerCreated, player } = this.state
+    const { playerCreated, player } = this.state;
     return (
       <div>
         {playerCreated ? (
           <Joystick player={player} />
         ) : (
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                Name:
-                <input
-                  type="text"
-                  value={this.state.name}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <input type="submit" value="Submit" />
+          <div className="rainbowBackground formBackground">
+            <h1 className="gameTitle">paintr</h1>
+            <form className="formContainer" onSubmit={this.handleSubmit}>
+              <input
+                className="nameInput"
+                placeholder="Enter Name"
+                type="text"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+              <input className="nameSubmit" type="submit" value="Submit" />
+              {/* <button className="nameSubmit" type="submit" value="Submit" /> */}
             </form>
           </div>
         )}
       </div>
-    )
+    );
   }
 }
-export default Controller
+export default Controller;
